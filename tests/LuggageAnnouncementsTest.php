0c7c5c39f5c21aa6acf9adfb6e29a21529fcc871
rlfrahm <frahmryan@gmail.com>
2014-10-09 14:00:33 -0500

    Adding tests for announcements

---
+++ tests/LuggageAnnouncementsTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author rlfrahm
+ * @date 10/6/14
+ * @time 3:32 PM
+ */
+class LuggageAnnouncementsTest extends PHPUnit_Extensions_Selenium2TestCase {
+//  protected function setUp() {
+//    global $argv;
+//    $this->setBrowser('firefox');
+//    $this->setBrowserUrl($argv[2]);
+//  }
+//
+//  /**
+//   * Tests the title
+//   */
+//  public function testPeoplePage() {
+//    $title = 'Announcements';
+//    $this->url('/announcements');
+//    $element = $this->byId('page-title');
+//    $this->assertEquals($title, $element->text());
+//  }
+}
